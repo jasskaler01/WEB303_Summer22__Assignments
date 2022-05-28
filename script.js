f02e570be8725ff6d388a51c7710a6f6daa54829
Jaskaran Singh <w0782371@myscc.ca>
2022-05-27 20:53:54 -0400

    Update script file with tax output

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 	WEB 303 Assignment 1 - jQuery
 	{Jaskaran singh}
 */
-$
+$(document).ready(function() {
+    $("#price").keyup(function() {
+        let getPrice = $("#price").val();
+        let tax = getPrice * 0.13;
+       
+
+	
+	$("#tax").text("$" + tax.toFixed(2));
+ });
+ });
