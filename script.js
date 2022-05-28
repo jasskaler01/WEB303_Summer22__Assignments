7a6588cb6cbdb8c03e495a4391f47ed0326fdad9
Jaskaran Singh <w0782371@myscc.ca>
2022-05-27 21:40:55 -0400

    Add list

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ $(document).ready(function() {
     $("#price").keyup(function() {
         let getPrice = $("#price").val();
         let tax = getPrice * 0.13;
-       
+		
 
-	
+		
 	$("#tax").text("$" + tax.toFixed(2));
+	$("#add-price").click(function(){
+		let totalPrice =+ getPrice +tax
+		$('#price-list').append("<li>" + totalPrice + "<li>")
+		console.log("Get Price = ", getPrice ,"Total price" , totalPrice)
+	})
  });
- });
+
+});
