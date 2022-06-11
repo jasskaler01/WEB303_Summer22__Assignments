392e90afc024a1a2fe09174936875ef7618ff279
Jaskaran Singh <w0782371@myscc.ca>
2022-06-11 18:03:48 -0400

    Create ajax method

--- Assignment3/js/script.js
+++ Assignment3/js/script.js
@@ -1,5 +1,6 @@
 // WEB303 Assignment 3
 // Jaskaran Singh
+$(function(){
 /*let getData =$.getJSON("data/glitchTeam.json").done(function(data){
     $.each(data.members,function(key,val){
 console.log("data Key",key,"And data val:",val)
@@ -14,5 +15,16 @@ $.ajax({
     beforeSend:function(){
         let LoadingTeam = $("<h1>Loading Team .......</h1>");
         $('div#glitch-team').append(LoadingTeam); 
+    },
+    error:function(){},
+    success:function(data){
+        $('div#glitch-team').empty(5000);
+        $.each(data.members,function(key,val){
+            console.log("data Key",key,"And data val:",val)
+            $('div#glitch-team').append(`<h4>${val.fullname}</h5
+            <h6>${val.position}</h6>
+            <p>${val.shortbio}</p>`)
+                })
+       
     }
-})})
+})})})
