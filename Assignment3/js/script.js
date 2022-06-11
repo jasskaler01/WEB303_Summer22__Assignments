274144b3f9773cea26a8b4c4d47985c5ccdf38f0
Jaskaran Singh <w0782371@myscc.ca>
2022-06-11 17:41:17 -0400

    create loading function

--- Assignment3/js/script.js
+++ Assignment3/js/script.js
@@ -1,10 +1,18 @@
 // WEB303 Assignment 3
 // Jaskaran Singh
-let getData =$.getJSON("data/glitchTeam.json").done(function(data){
+/*let getData =$.getJSON("data/glitchTeam.json").done(function(data){
     $.each(data.members,function(key,val){
 console.log("data Key",key,"And data val:",val)
 $('div#glitch-team').append(`<h4>${val.fullname}</h5
 <h6>${val.position}</h6>
 <p>${val.shortbio}</p>`)
     })
-})
+})*/
+$(function usingAjax(){
+$.ajax({
+    url:"data/glitchTeam.json",
+    beforeSend:function(){
+        let LoadingTeam = $("<h1>Loading Team .......</h1>");
+        $('div#glitch-team').append(LoadingTeam); 
+    }
+})})
