3235d76634220acd2872a434c007793322c762a3
Jaskaran Singh <w0782371@myscc.ca>
2022-06-11 17:28:41 -0400

    Create $.getJson to get data

--- Assignment3/js/script.js
+++ Assignment3/js/script.js
@@ -1,2 +1,10 @@
 // WEB303 Assignment 3
-// Your Name Here
+// Jaskaran Singh
+let getData =$.getJSON("data/glitchTeam.json").done(function(data){
+    $.each(data.members,function(key,val){
+console.log("data Key",key,"And data val:",val)
+$('div#glitch-team').append(`<h4>${val.fullname}</h5
+<h6>${val.position}</h6>
+<p>${val.shortbio}</p>`)
+    })
+})
